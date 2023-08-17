3e6b96d46f1ba4537314e8b7c49554440e73c2c4
Cristian Fernandez <cristian.fernandez@altovita.com>
2023-08-17 16:41:50 +0200

    tech: add body parser

--- backend/index.ts
+++ backend/index.ts
@@ -1,4 +1,5 @@
 import express, { Request, Response, Application } from "express";
+import bodyParser from "body-parser";
 import dotenv from "dotenv";
 
 import taskRouter from "./src/api/task";
@@ -9,7 +10,11 @@ dotenv.config();
 const app: Application = express();
 const port = process.env.PORT || 8000;
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(bodyParser.raw());
 app.use(taskRouter);
+
 app.get("/", (req: Request, res: Response) => {
   res.send("Welcome to Express & TypeScript Server");
 });
