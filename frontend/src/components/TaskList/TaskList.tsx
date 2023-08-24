ee829faa00573734303f80088eb5ba7eb6a6b07e
Cristian Fernandez <cristian.fernandez@altovita.com>
2023-08-25 00:17:32 +0200

    feat: add Redux

---
+++ frontend/src/components/TaskList/TaskList.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const TaskList: React.FC = () => {
+  return (
+    <div className="container mx-auto p-4">
+      <h1 className="text-3xl font-bold underline">Task Manager</h1>
+    </div>
+  );
+};
+
+export default TaskList;
